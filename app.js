bd63ae0d9d20377b83a73f0a6f2d1b8119e054d5
Ladvil <srihariharanb21@gmail.com>
2020-09-05 09:11:33 -0700

    cors

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require("dotenv/config")
 
 const app = express()
 
+//test
+const testroute = require("./test/test")
 const studentdetailroute = require("./router/studentdetail")
 const studentpost = require("./router/studentpost")
 const devroute = require('./router/developers/dev')
@@ -14,17 +16,34 @@ const adminpost = require("./router/admins/cases")
 
 
 app.use(bodyparser.json())
+app.use((req,res) =>{
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-requested-With, Content-Type, Accept, auth-token, Authorization"
+    )
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT,POST, PATCH, DELETE,GET')
+        return res.status(200).json({})
+    }
+})
 app.use('/dev', devroute)
 app.use('/students',studentdetailroute)
 app.use('/hw',studentpost)
 app.use('/admin', routeradmin)
 app.use('/cases',adminpost)
 
+//test
+app.use('/test', testroute)
+
+
 app.get("/", (req,res) => {
     res.send("antenna workin")
 })
 
 const url = process.env.url
+
+
 
 
 //const  connect  =  mongoose.connect(url, { useNewUrlParser: true , useUnifiedTopology: true })
